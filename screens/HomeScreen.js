679e4b51bba6d9b3501369a9c24f92d6477f064e
SodaCocacola <yackson.santos@escola.pr.gov.br>
2026-05-13 08:37:28 -0300

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -44,7 +44,7 @@ const RECEITAS_INICIAIS = [
  "6. Modele bolinhas e coloque na assadeira.",
  "7. Asse em forno médio até dourar."
     ]}, 
-    videoUrl: 'https://raw.githubusercontent.com/pedrojoinha2/3_D_ds_proverbios/main/videos/chocolate.mp4?raw=true'
+    videoUrl: 'https://raw.githubusercontent.com/SodaCocacola/SIM2/main/videos/chocolate.mp4?raw=true'
   },
   { 
     id: "2", 
@@ -84,7 +84,7 @@ const RECEITAS_INICIAIS = [
  "5. Misture até formar uma massa homogênea.",
  "6. Coloque na forma untada e leve ao forno médio por cerca de 30 minutos."
     ]}, 
-    videoUrl: 'https://raw.githubusercontent.com/pedrojoinha2/3_D_ds_proverbios/main/videos/fuba.mp4?raw=true' 
+    videoUrl: 'https://raw.githubusercontent.com/SodaCocacola/SIM2/main/videos/fuba.mp4?raw=true' 
   },
   { 
     id: "3", 
@@ -131,7 +131,7 @@ const RECEITAS_INICIAIS = [
  "2. Mexa até engrossar.",
  "3. Despeje sobre o bolo e finalize com granulado."
 ]}, 
-    videoUrl: 'https://raw.githubusercontent.com/pedrojoinha2/3_D_ds_proverbios/main/videos/cenoura.mp4?raw=true' 
+    videoUrl: 'https://raw.githubusercontent.com/SodaCocacola/SIM2/main/videos/cenoura.mp4?raw=true' 
   },
   { 
     id: "4", 
@@ -171,7 +171,7 @@ const RECEITAS_INICIAIS = [
  "5. Coloque sobre a massa já fria.",
  "6. Leve à geladeira até firmar."
  ]}, 
-    videoUrl: 'https://raw.githubusercontent.com/pedrojoinha2/3_D_ds_proverbios/main/videos/laranja.mp4?raw=true' 
+    videoUrl: 'https://raw.githubusercontent.com/SodaCocacola/SIM2/main/videos/laranja.mp4?raw=true' 
   },
 ];
 
@@ -182,7 +182,7 @@ const [carregando, setCarregando] = useState(false);
 const sincronizarReceitas = async () => {
 setCarregando(true);
 try {
-const response = await fetch('https://raw.githubusercontent.com/pedrojoinha2/3_D_ds_proverbios/refs/heads/main/receitas.json');
+const response = await fetch('https://raw.githubusercontent.com/SodaCocacola/SIM2/refs/heads/main/receitas.json');
 const receitasDoServidor = await response.json();
 const novas = receitasDoServidor.filter(resServidor => !receitas.some(resLocal => resLocal.id === resServidor.id));
 
